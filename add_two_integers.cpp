613c8100ffdecc0e4385d75ec27403e5c4fb46a7
tanish07858 <109678180+tanish07858@users.noreply.github.com>
2023-01-29 00:27:43 +0530

    Create add_two_integers.cpp

---
+++ add_two_integers.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int num1;
+    int num2;
+
+    cout<<"Enter 1st Number: ";
+    cin>>num1;
+
+    cout<<"Enter 2nd Number: ";
+    cin>>num2;
+
+    cout<<"The sum of "<<num1<<" and "<<num2<<" is: "<<(num1+num2);
+
+    return 0;
+
+}
