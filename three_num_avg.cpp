1e38b0682bce30f47c248eaddeb9d1d53b9981a4
tanish07858 <109678180+tanish07858@users.noreply.github.com>
2023-01-29 00:43:19 +0530

    Create three_num_avg.cpp

---
+++ three_num_avg.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;  
+
+int main(){
+    float num1;
+    float num2;
+    float num3;
+
+    cout<<"Enter 1st number: ";
+    cin>>num1;
+    cout<<"Enter 2st number: ";
+    cin>>num2;
+    cout<<"Enter 3st number: ";
+    cin>>num3;
+
+    cout<<"The average of the three number is : "<<(num1+num2+num3)/3.0;
+
+    return 0;
+}
