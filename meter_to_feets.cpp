40f82e7395b87268d9805adcc5aa6ee110f5d197
tanish07858 <109678180+tanish07858@users.noreply.github.com>
2023-01-29 00:31:16 +0530

    Create meter_to_feets.cpp

---
+++ meter_to_feets.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    float meters;
+
+    cout<<"Enter metric value: ";
+    cin>>meters;
+
+    cout<<"The feets value is: "<<(meters*3.28);
+
+    return 0;
+}
