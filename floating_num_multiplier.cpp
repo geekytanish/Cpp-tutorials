702ae03692acc480a4b99e7965d45d28af1b2f25
tanish07858 <109678180+tanish07858@users.noreply.github.com>
2023-01-29 00:39:51 +0530

    Create floating_num_multiplier.cpp

---
+++ floating_num_multiplier.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std; 
+
+int main(){
+    float floating_num_1;
+    float floating_num_2;
+
+    cout<<"Enter 1st floating number: ";
+    cin>>floating_num_1;
+
+    cout<<"Enter 2nd floating number: ";
+    cin>>floating_num_2;
+
+    cout<<"The product of the two numbers is : "<<(floating_num_1*floating_num_2);
+
+    return 0;
+
+
+}
