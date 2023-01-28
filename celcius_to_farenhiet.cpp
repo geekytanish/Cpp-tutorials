6a3a64a3bff6bcfd833ffbaab159018f83f84f0c
tanish07858 <109678180+tanish07858@users.noreply.github.com>
2023-01-29 00:36:00 +0530

    Create celcius_to_farenhiet.cpp

---
+++ celcius_to_farenhiet.cpp
@@ -0,0 +1,12 @@
+#include<iostream> 
+using namespace std;
+
+int main(){
+    float celcius;
+
+    cout<<"Enter celcius value: ";
+    cin>>celcius;
+
+    cout<<"The farenhiet value is: "<<((celcius*(9.0/5.0))+32.0);
+    return 0;
+}
