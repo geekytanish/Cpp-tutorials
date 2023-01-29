8561e9629a059153940e95ddcf8aa04d63749653
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-01-29 17:51:12 +0530

    Create even_num_without_if_else.cpp

---
+++ even_num_without_if_else.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std; 
+
+int main(){
+    int limit;
+
+    cout<<"Enter Limit: ";
+    cin>>limit;
+
+    for(int i=0;i<=limit;i=i+2){
+        cout<<i<<endl;
+    }
+
+    return 0;
+}
