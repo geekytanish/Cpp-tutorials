52f962e2370466f619a2f70b1adfe269a081be71
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-02-09 21:13:17 +0530

    Create continue_keyword.cpp

---
+++ continue_keyword.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int i=0; i<5; i=i+1){
+        cout<<"Good Morning"<<endl;
+        cout<<"Tanish is the best"<<endl;
+        continue;
+        cout<<"You can get better than tanish"<<endl;
+    }
+}
