3ba74f3de9d774741a3b71e746faf370dd7d7d79
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-02-09 19:01:15 +0530

    Create sum_of_n_while_loop.cpp

---
+++ sum_of_n_while_loop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter N: ";
+    cin>>n;
+
+    int sum=0;
+    int i=0;
+    while(i<=n){
+        sum=sum+i;
+        i=i+1;
+    }
+    cout<<"The sum is: "<<sum;
+    return 0;
+}
