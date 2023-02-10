c34b03b485bd579950bd051a72b7c692077b7e64
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-02-10 21:50:58 +0530

    Create indexing_in_array.cpp

---
+++ indexing_in_array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int ar[4]={1,3,5,7};
+  int n=0;
+
+  while(n!=4){
+    cout<<ar[n]<<endl;
+    n=n+1;
+  }
+  cout<<"Array initialised successfully"<<endl;
+  return 0;
+}
