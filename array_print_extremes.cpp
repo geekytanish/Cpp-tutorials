252ce00a1940f01cd5d2d2e97de21d392129548d
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-02-10 23:18:14 +0530

    Create array_print_extremes.cpp

---
+++ array_print_extremes.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+
+int main(){
+    int ar[5]={1,2,-3,9,5};
+    int n=5;
+    for(int i=0; i<n;i=i+1){
+        cout<<ar[i]<<" "<<ar[n-1]<<endl;
+        n=n-1;
+    }
+}
