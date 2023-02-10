655ce49ddcbcba41524e62c0e6bd4788ae1cc9fe
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-02-10 23:20:54 +0530

    Update array_print_extremes.cpp

--- array_print_extremes.cpp
+++ array_print_extremes.cpp
@@ -6,6 +6,8 @@ int main(){
     int ar[5]={1,2,-3,9,5};
     int n=5;
     for(int i=0; i<n;i=i+1){
+        if(ar[i]==ar[n-1]){cout<<ar[i];}
+        else
         cout<<ar[i]<<" "<<ar[n-1]<<endl;
         n=n-1;
     }
