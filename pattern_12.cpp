9cdec764ec50ebb629d3d6f4878bce98cba53f72
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-02-16 18:09:57 +0530

    Create pattern_12.cpp

---
+++ pattern_12.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char alpha='A';
+    int n;
+    cin>>n;
+    for(int row=0; row<n; row=row+1){
+        for(int col=0; col<=row; col=col+1){
+            cout<<alpha<<" ";
+            alpha++;
+        }
+        cout<<endl;
+    }
+}
