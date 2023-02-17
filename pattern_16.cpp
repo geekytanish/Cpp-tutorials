5685603bb31b7898311a5ad4b0bcea89d5fe9055
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-02-17 14:36:32 +0530

    Create pattern_16.cpp

---
+++ pattern_16.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int row=0; row<4; row=row+1){
+        //printing spaces 
+        for(int space=0; space<row; space =space +1) {
+            cout<<" ";
+        }
+        
+        //printing stars
+        for(int star=0; star<4-row; star=star+1){
+            cout<<"*";
+        }
+
+        cout<<endl;
+    }
+}
