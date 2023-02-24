fa0275e28a1ed46faa9512973f17959940abcd3b
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-02-25 00:04:54 +0530

    Create print_extremes.cpp

---
+++ print_extremes.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={1,2,3,4,5};
+
+    for(int i=0; i<5; i=i+1){
+        if(arr[i]==arr[5-i-1]){cout<<arr[i];
+        break;}
+        cout<<arr[i]<<" "<<arr[5-i-1]<<" ";
+    }
+}
