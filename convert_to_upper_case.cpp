d4c811a41e3ff38bff6d0075a972c5cdbdc9f1b1
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-02-25 13:21:26 +0530

    Create convert_to_upper_case.cpp

---
+++ convert_to_upper_case.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string.h>
+#include<array>
+using namespace std;
+
+int main(){
+    char arr[50];
+
+    cin.getline(arr, 50);
+
+    for(int i=0; i<strlen(arr); i=i+1){
+        arr[i]=arr[i]-32;
+    }
+    cout<<arr;
+}
