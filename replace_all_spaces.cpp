dab5ff5d3d7b7bf651e663d9c2c58db2b0417dc1
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-02-25 12:59:12 +0530

    Create replace_all_spaces.cpp

---
+++ replace_all_spaces.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string.h>
+#include<array>
+using namespace std;
+
+int main(){
+    char arr[50];
+
+    cin.getline(arr,50);
+
+    for(int i=0; i<strlen(arr); i=i+1){
+        if(arr[i]==' '){arr[i]='@';}
+    }
+
+    cout<<arr;
+}
