4114736a6742009b916a34dbf03d90b1e03ba06f
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-03-03 21:20:33 +0530

    Create pointers_basics.cpp

---
+++ pointers_basics.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=5;
+    cout<<"Printing value: "<<endl;
+    cout<<a<<endl;
+    cout<<"Printing Address: "<<endl;
+    cout<<&a<<endl;
+}
